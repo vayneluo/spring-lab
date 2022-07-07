0c8158086c88beaefd13ddca5fac9c27b9b69e3a
Vayne.Luo <vayluo@haizol.com>
2022-07-07 09:03:37 +0800

    feat: spring batch example

---
+++ spring-batch-lab/src/main/java/me/lattice/spring/batch/SpringBatchLabApplication.java
@@ -0,0 +1,13 @@
+package me.lattice.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchLabApplication.class, args);
+    }
+
+}
---
+++ spring-batch-lab/src/test/java/me/lattice/spring/batch/SpringBatchLabApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lattice.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
